8302886390651aecd47fd8381f335a458200ba18
lukancosta <lukancosta@hotmail.com>
2021-07-17 19:26:34 -0300

    UI of trip cards invitation

--- GoTripApplication/src/app/app.module.ts
+++ GoTripApplication/src/app/app.module.ts
@@ -2,23 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ActiveTripCardComponent } from './dashboard/active-trip-card/active-trip-card.component';
-=======
 import { NewTripFormComponent } from './new-trip-form/new-trip-form.component';
->>>>>>> bb6c0341113bfa4dffec922702a53c7b4342c993
 
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
     DashboardComponent,
     ActiveTripCardComponent,
-=======
     NewTripFormComponent,
->>>>>>> bb6c0341113bfa4dffec922702a53c7b4342c993
 
   ],
   imports: [
--- GoTripApplication/src/app/dashboard/dashboard.component.ts
+++ GoTripApplication/src/app/dashboard/dashboard.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-
+  userName: string = "John Warn";
   constructor() { }
 
   ngOnInit(): void {
