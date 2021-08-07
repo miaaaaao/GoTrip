03fe347f6f414c05cc3f0fc67d694c830fe4306a
Lucas Costa <lucascosta@Lucass-MacBook-Pro.local>
2021-08-07 15:20:23 -0300

    convert data to get the month name to include in the email url

--- ParseServer/cloud/sendInvitationEmail.js
+++ ParseServer/cloud/sendInvitationEmail.js
@@ -7,12 +7,17 @@ Parse.Cloud.define("sendInvitation", async (request)=>{
     console.log(request.user)
     let tripData = request.params;
     let user = request.user;
+    const months = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+]
 
     let listEmails = tripData.invitedFriends; // Array of emails
     let tripOwner = 'John'//request.User; // Get the user name
     let tripTitle = encodeURIComponent(tripData.title); // Remove empty space 
     let city = tripData.destination; // Get city name
-    let month = tripData.date.one.getMonth(); // convert data into month
+    let month = new Date(tripData.date.one).getMonth(); // convert data into month
+    let monthName = months[month]; // Get month name from months array
+
 
     try{
         for (let i = 0; i < listEmails.length; ++i){
@@ -20,7 +25,7 @@ Parse.Cloud.define("sendInvitation", async (request)=>{
                 to: listEmails[i].email,
                 from: "info@go-trip.tech",
                 subject: "You was invited to a Trip",
-                text: `Checkout this link for more information: http://127.0.0.1:4200/invitation?tripOwner=${tripOwner}#${tripTitle}#${city}#${month}`
+                text: `Checkout this link for more information: http://127.0.0.1:4200/invitation?tripOwner=${tripOwner}#${tripTitle}#${city}#${monthName}`
             })
         }
         
