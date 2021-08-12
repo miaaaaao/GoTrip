2f566673e46f5727be661fc29d40c294d104d421
Lucas Costa <lucascosta@Lucass-MacBook-Pro.local>
2021-08-12 09:32:28 -0300

    transfered basic trip information: city name and if the user is the owner of the trip to the cards

--- GoTripApplication/src/app/trip-details/all-sights/all-sights.component.ts
+++ GoTripApplication/src/app/trip-details/all-sights/all-sights.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { getTripDetails } from '../../services/getTripDetails.service';
 
 @Component({
   selector: 'app-all-sights',
@@ -6,15 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./all-sights.component.css']
 })
 export class AllSightsComponent implements OnInit {
+  city: string = '';
+  isTheOwner: boolean = false;
 
-  constructor() { 
+  constructor(private getTripDetails: getTripDetails) { 
+    
 
   }
 
   ngOnInit(): void {
-
+    this.getInitialdata()
   }
 
+  getInitialdata(){
+    this.city = this.getTripDetails.currentTrip.destination;
+    this.isTheOwner = this.getTripDetails.currentTrip.status.isTheOwner;
+  }
   
 
 }
--- GoTripApplication/src/app/trip-details/all-sights/sight-card/sight-card.component.ts
+++ GoTripApplication/src/app/trip-details/all-sights/sight-card/sight-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-sight-card',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sight-card.component.css']
 })
 export class SightCardComponent implements OnInit {
-
+  @Input() isTheOwner: boolean = false;
   constructor() { }
 
   ngOnInit(): void {
