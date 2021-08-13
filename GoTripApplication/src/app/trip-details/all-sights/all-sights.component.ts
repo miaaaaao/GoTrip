1e51adcd58ed6ee03f42be40057bb71b31b85a25
Lucas Costa <lucascosta@Lucass-MacBook-Pro.local>
2021-08-13 14:23:07 -0300

    Added next button on the page that shown sight cards

--- GoTripApplication/src/app/trip-details/all-sights/all-sights.component.ts
+++ GoTripApplication/src/app/trip-details/all-sights/all-sights.component.ts
@@ -21,13 +21,23 @@ export class AllSightsComponent implements OnInit {
   lon: number = 0;
   offset: number = 0; // offset from first object in the list
   limit: number = 3; // limit the number of results form the API
-  count: number = 0; // total objects count
   radius: number = 9000; // Area where the api will search for sights
   lang: string = 'en';
   country: string = 'DE';
   rate: number = 3; // How many starts it got.
 
+  count: number = 0; // total objects count
+  pageLength: number = 3 // number of cards per page
+  total: number = 0;
 
+  calculateTotalItensShown(){
+    if(this.count > 0){
+      this.total = this.offset + this.pageLength
+    } else {
+      this.total = 0
+    }
+    
+  }
   constructor(private getTripDetails: getTripDetails, private http: HttpClient, private env:env) { 
     
 
@@ -43,6 +53,7 @@ export class AllSightsComponent implements OnInit {
     this.isTheOwner = this.getTripDetails.currentTrip.status.isTheOwner;
     this.hasAcceptedInvitation = this.getTripDetails.currentTrip.status.hasAcceptedInvitation
   }
+
 
   /*
   * This function Fetch the lon and lat of the city
@@ -115,7 +126,7 @@ export class AllSightsComponent implements OnInit {
        }
       }, 2000); 
       }
-
+      this.calculateTotalItensShown()
       console.log(resp)
       
     })
@@ -145,6 +156,18 @@ export class AllSightsComponent implements OnInit {
       this.listOfSights = [...this.listOfSights, newSight ]
       console.log(this.listOfSights)
     })
+  }
+
+  /*
+  * Get data for the next page
+  */
+  nextPage(){
+    this.offset = this.offset + this.pageLength; // calculate the next
+    this.listOfSights = [] // Remove itens of previous search from the list
+    this.getSightList(); // Get next sights
+    this.calculateTotalItensShown() // calculate the total sight shown to update the button
+    console.log(this.offset)
+    console.log(this.total)
   }
 
 }
