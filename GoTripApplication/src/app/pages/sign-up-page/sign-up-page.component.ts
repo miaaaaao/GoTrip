a0a410bdfc9672f5aa22f3d48e913b6069d4754f
Belen Gimenez <belengimenezcic@gmail.com>
2021-08-19 11:26:40 +0200

    Some advances in the sign up and login forms

--- GoTripApplication/src/app/pages/sign-up-page/sign-up-page.component.ts
+++ GoTripApplication/src/app/pages/sign-up-page/sign-up-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as Parse from 'parse';
 
 @Component({
   selector: 'app-sign-up-page',
@@ -6,10 +7,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-up-page.component.css']
 })
 export class SignUpPageComponent implements OnInit {
+  email: string = ''
+  password: string = ''
+  username: string = ''
 
   constructor() { }
 
   ngOnInit(): void {
   }
+  onKey(event: any) { // without type info
+  //  this.email = event.target.value
+  }
+  async onClickMe() {
+    const user = new Parse.User();
+    user.set("username", this.username);
+    user.set("password", this.password);
+    user.set("email", this.email);
+
+    console.log('test');
+
+
+       try {
+          await user.signUp();
+          // Hooray! Let them use the app now.
+        } catch (error) {
+          // Show the error message somewhere and let the user try again.
+          alert("Error: " + error.code + " " + error.message);
+        }
+
+
+  }
+
+
+
+
+
 
 }
