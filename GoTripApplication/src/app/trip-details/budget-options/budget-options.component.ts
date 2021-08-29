3b13eb66f257edb603cdb8bab9f707cd180529d9
Lucas Costa <lucascosta@Lucass-MacBook-Pro.local>
2021-08-29 12:14:14 -0300

    added a function and UI element to calculate the budget winner based on the votes

--- GoTripApplication/src/app/trip-details/budget-options/budget-options.component.ts
+++ GoTripApplication/src/app/trip-details/budget-options/budget-options.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, DoCheck } from '@angular/core';
 
 import { VoteBudgetService } from '../../services/vote-budget.service';
 
@@ -7,11 +7,71 @@ import { VoteBudgetService } from '../../services/vote-budget.service';
   templateUrl: './budget-options.component.html',
   styleUrls: ['./budget-options.component.css']
 })
-export class BudgetOptionsComponent implements OnInit {
+export class BudgetOptionsComponent implements OnInit, DoCheck {
   @Input() currentTrip:any;
+  winner: number = -1;
 
   constructor(private voteBudgetService: VoteBudgetService) { 
     
+  }
+
+  getDuplicateArrayElements(arr:any){
+    var sorted_arr = arr.slice().sort();
+    var results = [];
+    for (var i = 0; i < sorted_arr.length - 1; i++) {
+        if (sorted_arr[i + 1] === sorted_arr[i]) {
+            results.push(sorted_arr[i]);
+        }
+    }
+    return results;
+}
+
+
+  winnerBudget(){
+    let totalMembers = this.currentTrip.invitedFriends.length;
+    let voteOne = this.currentTrip.budget.totalVote.one;
+    let voteTwo = this.currentTrip.budget.totalVote.two;
+    let voteThree = this.currentTrip.budget.totalVote.three;
+
+    if(totalMembers == voteOne + voteTwo + voteThree){
+      let allVotes:number[] = [voteOne, voteTwo ,voteThree];
+      let largestValue:number = Math.max(...allVotes);
+
+      //Check if there is repeated votes
+      let hasDuplicate = allVotes.some((val, i) => allVotes.indexOf(val) !== i);
+      let calculateWinner = true;
+
+      if(hasDuplicate){
+        let duplicatedList = this.getDuplicateArrayElements(allVotes); // return a array of numbers that are duplicated
+
+        if(duplicatedList[0] < largestValue){
+          calculateWinner = true // if there are repeated votes [1,1] and the largest number is 2, we can calculate the winner 
+
+        } else if (duplicatedList[0] == largestValue){
+          calculateWinner = false // if there are repeated votes [2,2] and they are the largest number, the winner can not be calculated untill the team find a democratic budget.
+        }
+      }
+     
+     
+      //Store the winner
+      if(calculateWinner){
+        this.winner = allVotes.findIndex(el=> el == largestValue);
+      }
+      
+
+    }else {
+      
+      this.winner = -1; // no winner
+      return
+    }
+
+  }
+
+  ngDoCheck(){
+    this.winner = -1;
+    if(this.currentTrip.invitedFriends.length > 0){
+      this.winnerBudget()
+    } 
   }
 
   ngOnInit(): void {
@@ -31,7 +91,9 @@ export class BudgetOptionsComponent implements OnInit {
         }
       }
     }
+    
   }
+
 
   saveBudgetPreferences(option:string){
     
