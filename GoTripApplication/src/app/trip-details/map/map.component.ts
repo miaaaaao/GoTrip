1d526b1b690dcf437819b9fb51c9a5bf96e0ef82
Belen Gimenez <belengimenezcic@gmail.com>
2021-09-01 19:16:01 +0200

    Small cleanup of the map code

--- GoTripApplication/src/app/trip-details/map/map.component.ts
+++ GoTripApplication/src/app/trip-details/map/map.component.ts
@@ -7,35 +7,35 @@ declare var H: any;
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css']
 })
+
 export class MapComponent implements OnInit {
   title = 'here-project';
   private platform: any;
 
-   @ViewChild("map")
-   public mapElement!: ElementRef;
+  @ViewChild("map")
+  public mapElement!: ElementRef;
 
 
   public constructor() {
 
-   this.platform = new H.service.Platform({
-      "apikey": "HERE_API"
+    this.platform = new H.service.Platform({
+      'apikey': '{HERE_API}'
     });
   }
 
   public ngOnInit(): void { }
 
-  public  ngAfterViewInit() {
+  public ngAfterViewInit() {
+
     let defaultLayers = this.platform.createDefaultLayers();
+
     let map = new H.Map(
       this.mapElement.nativeElement,
       defaultLayers.vector.normal.map,
       {
         zoom: 10,
         center: { lat: 51.233334, lng: 6.78333 }
-      }
-    );
+      });
     console.log(map)
   }
-
-
 }
