cf589104ed995751b9f6cd5637578341096ca5cd
Lucas Costa <lucascosta@Lucass-MacBook-Pro.local>
2021-09-09 14:29:38 -0300

    Fucntion to get added sights to the trip from Parse server

---
+++ GoTripApplication/src/app/services/get-added-sight.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import * as Parse from 'parse';
+
+import { getTripDetails } from  './getTripDetails.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GetAddedSightService {
+
+  addedSights:{}[] = [];
+
+  constructor(private getTripDetails: getTripDetails) { }
+
+  async getSights(){
+    let Sight = Parse.Object.extend('Sight');
+    const querySights = new Parse.Query(Sight);
+
+    let TripPlan = Parse.Object.extend('TripsPlan');
+    let tripPlan = new TripPlan();
+    tripPlan.id = this.getTripDetails.currentTrip.id;
+   
+    querySights.equalTo('tripsPlanId', tripPlan);
+    querySights.equalTo('addedToTrip', true);
+
+    /*
+    * Get the sights that were added to the trip
+    */
+    await querySights.find()
+    .then((results)=>{
+      for(let i = 0; i < results.length; i++){
+        this.addedSights.push({
+          title: results[i].get('name'),
+          xid: results[i].get('xid'),
+          geoPoint: results[i].get('geoPoint'),
+        })
+      }
+      console.log(this.addedSights)
+    })
+    .catch(function(error) {
+      console.log(error)
+    });
+  }
+}
--- GoTripApplication/src/app/trip-details/map/map.component.ts
+++ GoTripApplication/src/app/trip-details/map/map.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { getTripDetails } from '../../services/getTripDetails.service';
 
+import { GetAddedSightService } from '../../services/get-added-sight.service';
+
 declare var L: any;
 
 @Component({
@@ -12,26 +14,29 @@ declare var L: any;
 export class MapComponent implements OnInit {
   public map: any;
   isTheOwner: boolean = false;
+  geoLocation: any;
+  allSights: any;
 
-  public constructor(private getTripDetails: getTripDetails) {
-   
+  public constructor(private getTripDetails: getTripDetails, private getAddedSightService: GetAddedSightService) {
+    this.geoLocation = this.getTripDetails.currentTrip.geoLocation; // Save current city geolocation
   }
 
   public ngOnInit() { 
     this.isTheOwner = this.getTripDetails.currentTrip.status.isTheOwner; // Search if the user is the owner. If yes he can remove the sights
 
+    //Get all added sights
+    this.getAddedSightService.getSights() 
+    this.allSights = this.getAddedSightService.addedSights;
+    
+    //Start the map
     this.map = L.map('map', {
       scrollWheelZoom: false,
-    }).setView([51.1657, 10.4515], 15);
+    }).setView([this.geoLocation.lat, this.geoLocation.lon], 15);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(this.map);
 
-  }
-
-  public ngAfterViewInit() {
-      
   }
 
 }
